da796184586d36c374f936e888f3178d9acfc878
Ivan Somkin <somkin.van@yandex.ru>
2022-09-10 06:41:33 +0300

    Upload static

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  
+  const configService: ConfigService = app.get(ConfigService);
+  await app.listen(configService.get('PORT') || 8000);
+}
+bootstrap();
